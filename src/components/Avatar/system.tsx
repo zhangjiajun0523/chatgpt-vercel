7d0b859a678c1e54912f3ed7133882ca22ba88d8
Thek28 <43231469+zhangjiajun0523@users.noreply.github.com>
2023-04-21 10:11:12 +0800

    Update system.tsx

--- src/components/Avatar/system.tsx
+++ src/components/Avatar/system.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const SystemAvatar: FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
-  <img width={32} height={32} {...props} src="/openai.webp" />
+  <img width={32} height={32} {...props} src="/assistant-avatar.gif" />
 );
 
 export default SystemAvatar;
