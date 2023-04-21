201cb2d4378643f8c0228ef45473f9e47bc41b3a
Thek28 <43231469+zhangjiajun0523@users.noreply.github.com>
2023-04-21 10:04:09 +0800

    Update system.tsx

--- src/components/Avatar/system.tsx
+++ src/components/Avatar/system.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const SystemAvatar: FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
-  <img width={32} height={32} {...props} src="https://vip.chargpt.cn/_astro/assistant-avatar.55eb2217.gif" />
+  <img width={32} height={32} {...props} src="/assistant-avatar.gif" />
 );
 
 export default SystemAvatar;
